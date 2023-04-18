57e08ff825435426230dd08703c6e381d9babcc5
MiXACT <out_lawz@mail.ru>
2023-04-18 20:08:56 +0300

    tooltip test

--- html_forms/tooltip.test.js
+++ html_forms/tooltip.test.js
@@ -1,84 +1,53 @@
-// import puppeteer from 'puppeteer';
-// import { fork } from 'child_process';
+import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
 
-// // eslint-disable-next-line
-// import setimmediate from 'setimmediate';
-// jest.setTimeout(30000); // default puppeteer timeout
+// eslint-disable-next-line
+import setimmediate from 'setimmediate';
+jest.setTimeout(50000); // default puppeteer timeout
 
-// describe('Credit Card Validator form', () => {
-// 	let browser = null;
-// 	let page = null;
-// 	let server = null;
-// 	const baseUrl = 'http://localhost:9000';
+describe('Tooltip test', () => {
+	let browser = null;
+	let page = null;
+	let server = null;
+	const baseUrl = 'http://localhost:9000';
 
-// 	beforeAll(async () => {
-// 		server = fork(`${__dirname}/e2e.server.js`);
-// 		await new Promise((resolve, reject) => {
-// 			server.on('error', reject);
-// 			server.on('message', (message) => {
-// 				if (message === 'ok') {
-// 					resolve();
-// 				}
-// 			});
-// 		});
-// 		const browserFetcher = puppeteer.createBrowserFetcher();
-// 		const revisionInfo = await browserFetcher.download('1095492');
+	beforeAll(async () => {
+		server = fork(`${__dirname}/tooltip.server.js`);
+		await new Promise((resolve, reject) => {
+			server.on('error', reject);
+			server.on('message', (message) => {
+				if (message === 'ok') {
+					resolve();
+				}
+			});
+		});
+		const browserFetcher = puppeteer.createBrowserFetcher();
+		const revisionInfo = await browserFetcher.download('1095492');
 
-// 		browser = await puppeteer.launch({
-// 			executablePath: revisionInfo.executablePath,
-// 			// ignoreDefaultArgs: ['--disable-extensions'],
-// 			headless: true,
-// 			// args: ['--no-sandbox', '--disabled-setupid-sandbox'],
-// 		});
-// 		// browser = await puppeteer.launch({
-// 		// 	// executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome',
-// 		// 	// headless: false, // show gui
-// 		// 	// slowMo: 250,
-// 		// 	// devtools: true, // show devTools
-// 		// });
-// 		page = await browser.newPage();
-// 	});
+		browser = await puppeteer.launch({
+			executablePath: revisionInfo.executablePath,
+			// ignoreDefaultArgs: ['--disable-extensions'],
+			headless: true,
+			// args: ['--no-sandbox', '--disabled-setupid-sandbox'],
+		});
+		// browser = await puppeteer.launch({
+		// 	executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome',
+		// 	headless: false, // show gui
+		// 	slowMo: 250,
+		// 	devtools: true, // show devTools
+		// });
+		page = await browser.newPage();
+	});
 
-// 	afterAll(async () => {
-// 		await browser.close();
-// 		server.kill();
-// 	});
+	afterAll(async () => {
+		await browser.close();
+		server.kill();
+	});
 
-// 	test('test card types', async () => {
-// 		const cards = {
-// 			mir: '2200220022002222',
-// 			diners_club: '30328079445028',
-// 			jcb: '3532590359720076',
-// 			amex: '373281720024152',
-// 			visa: '4716248828278863',
-// 			master: '5314613736666777',
-// 			discover: '6011134319283934',
-// 		};
-// 		await page.goto(baseUrl);
-// 		const div = await page.$('.col-md-5');
-// 		const input = await div.$('.form-control');
-// 		for (const cardType in cards) {
-// 			if (Object.prototype.hasOwnProperty.call(cards, cardType)) {
-// 				/* eslint-disable */
-// 				await input.type(cards[cardType]);
-// 				await page.waitForSelector(`.${cardType} .cdisabled`, { hidden: true });
-// 				/* eslint-enable */
-// 			}
-// 		}
-// 	});
-
-// 	test('test card number', async () => {
-// 		await page.goto(baseUrl);
-// 		const div = await page.$('.col-md-5');
-// 		const input = await div.$('.form-control');
-// 		const submit = await div.$('.btn');
-
-// 		await input.type('4967538166331332');
-// 		await submit.click();
-// 		await page.waitForSelector('#c_valid', { visible: true });
-
-// 		await input.type('1111111111111111');
-// 		await submit.click();
-// 		await page.waitForSelector('#c_invalid', { visible: true });
-// 	});
-// });
+	test('test popover', async () => {
+		await page.goto(baseUrl);
+		const btn = await page.$('.btn');
+		await btn.click();
+		await page.waitForSelector('.popover', { visible: true });
+	});
+});
